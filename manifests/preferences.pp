398fca2909a73e4090a2fa61a235568e89bced80
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2011-11-15 10:33:03 +0100

    apt::preferences: fixed apt version condition for future releases
    Replaced explicit releases naming with version comparison to avoid having to
    list all future releases in there. This seems more reasonable.

--- manifests/preferences.pp
+++ manifests/preferences.pp
@@ -8,28 +8,27 @@ define apt::preferences($ensure="present", $package="", $pin, $priority) {
   $fname = regsubst($name, '\.', '-', 'G')
 
   # apt support preferences.d since version >= 0.7.22
-  case $lsbdistcodename {
-    /lucid|squeeze/ : {   
-      file {"/etc/apt/preferences.d/$fname":
-        ensure  => $ensure,
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        content => template("apt/preferences.erb"),
-        before  => Exec["apt-get_update"],
-        notify  => Exec["apt-get_update"],
-      }
-
-    }
-    default: {
-      common::concatfilepart { $fname:
-        ensure  => $ensure,
-        manage  => true,
-        file    => "/etc/apt/preferences",
-        content => template("apt/preferences.erb"),
-        before  => Exec["apt-get_update"],
-        notify  => Exec["apt-get_update"],
-      }
+  if ($lsbdistid == "Debian" and versioncmp($lsbdistrelease, "6.0") >= 0) or
+     ($lsbdistid == "Ubuntu" and versioncmp($lsbdistrelease, "10.4") >= 0) {
+    file {"/etc/apt/preferences.d/$fname":
+      ensure  => $ensure,
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      content => template("apt/preferences.erb"),
+      before  => Exec["apt-get_update"],
+      notify  => Exec["apt-get_update"],
     }
   }
+  else {
+    common::concatfilepart { $fname:
+      ensure  => $ensure,
+      manage  => true,
+      file    => "/etc/apt/preferences",
+      content => template("apt/preferences.erb"),
+      before  => Exec["apt-get_update"],
+      notify  => Exec["apt-get_update"],
+    }
+  }
+
 }
