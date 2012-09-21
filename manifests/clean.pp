c16baeebcfb6d01afb83633a4b4ee3b4b26f238f
Steve Huff <shuff@vecna.org>
2012-09-21 12:23:56 -0400

    fixing potentially invalid monthday parameter
    The "monthday" parameter of cron resources cannot be 0; however, the
    code that generates a random value for that parameter in apt::clean can
    sometimes generate a 0 value.  This patch changes the behavior slightly
    in order to generate values between 1 and 28 inclusive.

--- manifests/clean.pp
+++ manifests/clean.pp
@@ -4,14 +4,14 @@
 #
 # === Variables
 #
-#  *$apt_clean_minutes*: cronjob minutes  - default uses fqdn_rand()
+#  *$apt_clean_minutes*: cronjob minutes  - default uses fqdn_rand(), range 0 to 59
 #  *$apt_clean_hours*:   cronjob hours    - default to 0
-#  *$apt_clean_mday*:    cronjob monthday - default uses fqdn_rand()
+#  *$apt_clean_mday*:    cronjob monthday - default uses fqdn_rand(), range 1 to 29
 #
 class apt::clean {
   $minutes  = $apt_clean_minutes? {'' => fqdn_rand(60), default => $apt_clean_minutes }
   $hours    = $apt_clean_hours?   {'' => '0'          , default => $apt_clean_hours }
-  $monthday = $apt_clean_mday?    {'' => fqdn_rand(29), default => $apt_clean_mday }
+  $monthday = $apt_clean_mday?    {'' => fqdn_rand(28) + 1, default => $apt_clean_mday }
 
   cron {'cleanup APT cache - prevents diskfull':
     ensure   => present,
